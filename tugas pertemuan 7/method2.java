86021d6344fc80f10e89114dfd2d25240a1e06c2
reza7angkasa <72240159+reza7angkasa@users.noreply.github.com>
2020-10-30 23:18:49 +0700

    Add files via upload

---
+++ tugas pertemuan 7/method2.java
@@ -0,0 +1,62 @@
+class method2 {
+    public int A;
+    public int B;
+    public int C;
+    public int D;
+    public int E;
+    public double F;
+    public double G;
+
+    public void setA(int A) {
+        this.A = A;
+    }
+
+    public void setB(int B) {
+        this.B = B;
+    }
+    public void setC(int C) {
+        this.C = C;
+    }
+
+    public void setD(int D) {
+        this.D = D;
+    }
+    public void setE(int E) {
+        this.E = E;
+    }
+
+    public void setF(double F) {
+        this.F = F;
+    }
+    public void setG(double G) {
+        this.G = G;
+    }
+
+
+    public int getA() {
+        return this.A;
+    }
+
+    public int getB() {
+        return this.B;
+    }
+    public int getC() {
+        return this.C;
+    }
+
+    public int getD() {
+        return this.D;
+    }
+    public int getE() {
+        return this.E;
+    }
+
+    public double getF() {
+        return this.F;
+    }
+    public double getG() {
+        return this.G;
+    }
+
+}
+
