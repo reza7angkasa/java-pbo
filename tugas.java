1c67c75c047d3244ab8a3c59173cd0cec020a700
Angkasas Pride <ardeanreza7@gmail.com>
2020-12-05 10:48:32 +0700

    login system

--- tugas.java
+++ tugas.java
@@ -2,34 +2,33 @@ package com.company;
 import java.util.*;
 
 public class tugas {
-    public static void main(String[] args) {
+ static int dicari(int A[],int cari){
+        for(int i=0; i<A.length; i++){
+            if(A[i]==cari){
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    public static void main (String []   args) {
         String username1 = "AngkasaGanteng";
         String password1 = "4ngk454";
-        {
-            Scanner input = new Scanner(System.in);
+
+        Scanner cc = new Scanner(System.in);
+        int A[] = new int[2];
+        for (int i = 0; i < A.length; i++) {
             System.out.print("Username: ");
-            String username = input.nextLine();
+            String username = cc.nextLine();
             System.out.print("Password: ");
-            String password = input.nextLine();
+            String password = cc.nextLine();
             if ((username.equals(username1)) && (password.equals(password1))) {
                 System.out.println("Loggin Success");
                 System.exit(0);
             } else {
                 System.out.println("Try Again");
-            }}
-        {
-            Scanner input = new Scanner(System.in);
-            System.out.print("Username: ");
-            String username = input.nextLine();
-            System.out.print("Password: ");
-            String password = input.nextLine();
-            if ((username.equals(username1)) && (password.equals(password1))) {
-                System.out.println("Loggin Success");
-                System.exit(0);
-            }else{
-                System.out.println("Try Again");
-            }}
+            }
+        }
         Scanner input = new Scanner(System.in);
         System.out.print("Username: ");
         String username = input.nextLine();
